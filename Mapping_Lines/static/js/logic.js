35f900eef125372b1bbdc12711a650c4fec66c49
Stuti Budhwar <stutibudhwar3@gmail.com>
2020-10-06 22:23:18 -0700

    updated logic.js

--- Mapping_Lines/static/js/logic.js
+++ Mapping_Lines/static/js/logic.js
@@ -1,8 +1,21 @@
 // Add console.log to check to see if our code is working.
 console.log("working");
 
-// Create the map object with a center and zoom level.
-let map = L.map('mapid').setView([40.7, -94.5], 4);
+// Create the map object with center at the San Francisco airport.
+let map = L.map('mapid').setView([37.6213, -122.3790], 5);
+
+// Coordinates for each point to be used in the line, LA to SF here
+let line = [
+  [33.9416, -118.4085],
+  [37.6213, -122.3790],
+  [40.7899, -111.9791],
+  [47.4502, -122.3088]
+];
+
+// Create a polyline using the line coordinates and make the line red.
+L.polyline(line, {
+  color: "yellow"
+}).addTo(map);
 
 // Get data from cities.js, assigned variable cityData to array cities
 let cityData = cities;
