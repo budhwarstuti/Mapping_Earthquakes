f0217f905ec2d85b26d61e90d9f83d69d346aef0
Stuti Budhwar <stutibudhwar31@gmail.com>
2020-10-07 20:22:52 -0700

    updated tectonic plate layer on map

--- Earthquake_Challenge/static/js/challenge_logic.js
+++ Earthquake_Challenge/static/js/challenge_logic.js
@@ -30,11 +30,13 @@ let baseMaps = {
 
 // 1. Add a 2nd layer group for the tectonic plate data.
 let allEarthquakes = new L.LayerGroup();
+let tectonicplates = new L.LayerGroup();
 
 
 // 2. Add a reference to the tectonic plates group to the overlays object.
 let overlays = {
-  "Earthquakes": allEarthquakes
+  "Earthquakes": allEarthquakes,
+  "Tectonic Plates": tectonicplates
 };
 
 // Then we add a control to the map that will allow the user to change which
@@ -141,7 +143,12 @@ legend.onAdd = function() {
 
 
   // 3. Use d3.json to make a call to get our Tectonic Plate geoJSON data.
-  d3.json().then(() {
+  d3.json().then("https://raw.githubusercontent.com/fraxen/tectonicplates/master/GeoJSON/PB2002_boundaries.json").then(function(tPlatesData) {
+    L.geoJson(tPlatesData, {
+      color: "ff6500",
+      weight: 2
+    }).addTo(tectonicplates)
+    tectonicplates.addTo(map)
+  })
     
-  });
-});
+})
